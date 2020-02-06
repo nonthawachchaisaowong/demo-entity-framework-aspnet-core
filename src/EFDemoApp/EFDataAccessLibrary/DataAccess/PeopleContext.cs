3de0a11aaed5f28d4e43801128100300b564fe82
Nonthawach Chaisaowong <nonthawach.chaisaowong@gmail.com>
2020-02-06 17:14:40 +0700

    Add model validation

---
+++ src/EFDemoApp/EFDataAccessLibrary/DataAccess/PeopleContext.cs
@@ -0,0 +1,13 @@
+﻿using EFDataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFDataAccessLibrary.DataAccess
+{
+    public class PeopleContext : DbContext
+    {
+        public PeopleContext(DbContextOptions options) : base(options) { }
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Email> EmailAddresses { get; set; }
+    }
+}
